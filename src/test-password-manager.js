68b1eb7971329076ea59a76765a28e59ce73f63b
George H. Alvares <blackbird.fly95@gmail.com>
2015-06-05 00:39:14 -0300

    a few tests

--- src/test-password-manager.js
+++ src/test-password-manager.js
@@ -7,33 +7,44 @@ function assert(condition, message) {
   }
 }
 
+var lib = require("./lib/lib");
 var password_manager = require("./password-manager");
 
-var password = "password123!!";
+var password = "auJKAjkxnasklNcauaLK";
 var keychain = password_manager.keychain();
 
 console.log("Initializing a toy password store");
 keychain.init(password);
 
-var kvs = { "service1": "value1", 
-            "service2": "value2",
-            "service3": "value3" };
+// Random data
+var kvs = {};
+
+var d, p;
+for (var i = 0; i < 10000; i++) {
+	d = lib.bitarray_to_string(unescape(encodeURIComponent(lib.random_bitarray(32))));
+	p = lib.bitarray_to_string(unescape(encodeURIComponent(lib.random_bitarray(32))));
+
+	kvs[d] = p;
+}
+
+console.log("\n------ Testing");
 
 console.log("Adding keys to password manager");
 for (var k in kvs) {
   keychain.set(k, kvs[k]);
 }
 
-console.log("Testing get");
+console.log("Testing 'get'");
 for (var k in kvs) {
   assert(keychain.get(k) === kvs[k], "Get failed for key " + k);
 }
-assert(keychain.get("service4") === null);
 
-console.log("Testing remove");
-assert(keychain.remove("service1"));
-assert(!keychain.remove("service4"));
-assert(keychain.get("service4") === null);
+console.log("Testing 'remove'");
+for (var k in kvs) {
+	assert(keychain.remove(k));
+	assert(!keychain.remove(k));
+	assert(!keychain.get(k));
+}
 
 console.log("Saving database");
 var data = keychain.dump();
@@ -49,5 +60,45 @@ console.log("Checking contents of new database");
 for (var k in kvs) {
   assert(keychain.get(k) === new_keychain.get(k));
 }
+
+console.log("Testing for authentication tampering:")
+
+console.log("1) Wrong password");
+new_keychain = password_manager.keychain();
+assert(!new_keychain.load("adqAUdqn", contents, cksum));
+
+console.log("2) Invalid signature")
+var contentsObj = JSON.parse(contents);
+contentsObj["signature"][0] = 0;
+
+var tampContent = JSON.stringify(contentsObj);
+new_keychain = password_manager.keychain();
+assert(!new_keychain.load(password, tampContent, cksum));
+
+console.log("3) Wrong hash");
+var tampHash = lib.SHA256(tampContent);
+new_keychain = password_manager.keychain();
+
+var r;
+try {
+	new_keychain.load(password, contents, tampHash);
+	r = false;
+} catch(e) {
+	r = true;
+}
+assert(r);
+
+console.log("4) Wrong salt")
+contentsObj = JSON.parse(contents);
+contentsObj["salt"][2] = 0;
+
+tampContent = JSON.stringify(contentsObj);
+new_keychain = password_manager.keychain();
+assert(!new_keychain.load(password, tampContent, cksum));
+
+/* TODO:
+ * 1) Swap Attack
+ * 2) Rollback Attack
+ */
 
 console.log("All tests passed!");
